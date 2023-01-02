beedae84b1b602e28a9a1719a86a2fdb3ea489e2
Astro_815 <astro815sfgKW@gmail.com>
2023-01-02 17:44:47 -0300

    Update hi.js

--- hi.js
+++ hi.js
@@ -1,137 +1,245 @@
-const Cast = require('../util/cast');
+// Core, Team, and Official extensions can `require` VM code:
+const ArgumentType = require('../../extension-support/argument-type');
+const BlockType = require('../../extension-support/block-type');
+const TargetType = require('../../extension-support/target-type');
 
-class Scratch3EventBlocks {
-    constructor (runtime) {
+// ...or VM dependencies:
+const formatMessage = require('format-message');
+
+// Core, Team, and Official extension classes should be registered statically with the Extension Manager.
+// See: scratch-vm/src/extension-support/extension-manager.js
+class SomeBlocks {
+    constructor(runtime) {
         /**
-         * The runtime instantiating this block package.
+         * Store this for later communication with the Scratch VM runtime.
+         * If this extension is running in a sandbox then `runtime` is an async proxy object.
          * @type {Runtime}
          */
         this.runtime = runtime;
-
-        this.runtime.on('KEY_PRESSED', key => {
-            this.runtime.startHats('event_whenkeypressed', {
-                KEY_OPTION: key
-            });
-            this.runtime.startHats('event_whenkeypressed', {
-                KEY_OPTION: 'any'
-            });
-        });
     }
 
     /**
-     * Retrieve the block primitives implemented by this package.
-     * @return {object.<string, Function>} Mapping of opcode to Function.
+     * @return {object} This extension's metadata.
      */
-    getPrimitives () {
+    getInfo() {
         return {
-            event_whentouchingobject: this.touchingObject,
-            event_broadcast: this.broadcast,
-            event_broadcastandwait: this.broadcastAndWait,
-            event_whengreaterthan: this.hatGreaterThanPredicate
-        };
-    }
+            // Required: the machine-readable name of this extension.
+            // Will be used as the extension's namespace.
+            // Allowed characters are those matching the regular expression [\w-]: A-Z, a-z, 0-9, and hyphen ("-").
+            id: 'someBlocks',
 
-    getHats () {
-        return {
-            event_whenflagclicked: {
-                restartExistingThreads: true
-            },
-            event_whenkeypressed: {
-                restartExistingThreads: false
-            },
-            event_whenthisspriteclicked: {
-                restartExistingThreads: true
-            },
-            event_whentouchingobject: {
-                restartExistingThreads: false,
-                edgeActivated: true
-            },
-            event_whenstageclicked: {
-                restartExistingThreads: true
-            },
-            event_whenbackdropswitchesto: {
-                restartExistingThreads: true
-            },
-            event_whengreaterthan: {
-                restartExistingThreads: false,
-                edgeActivated: true
-            },
-            event_whenbroadcastreceived: {
-                restartExistingThreads: true
-            }
-        };
-    }
+            // Core extensions only: override the default extension block colors.
+            color1: '#FF8C1A',
+            color2: '#DB6E00',
 
-    touchingObject (args, util) {
-        return util.target.isTouchingObject(args.TOUCHINGOBJECTMENU);
-    }
+            // Optional: the human-readable name of this extension as string.
+            // This and any other string to be displayed in the Scratch UI may either be
+            // a string or a call to `formatMessage`; a plain string will not be
+            // translated whereas a call to `formatMessage` will connect the string
+            // to the translation map (see below). The `formatMessage` call is
+            // similar to `formatMessage` from `react-intl` in form, but will actually
+            // call some extension support code to do its magic. For example, we will
+            // internally namespace the messages such that two extensions could have
+            // messages with the same ID without colliding.
+            // See also: https://github.com/yahoo/react-intl/wiki/API#formatmessage
+            name: formatMessage({
+                id: 'extensionName',
+                defaultMessage: 'Some Blocks',
+                description: 'The name of the "Some Blocks" extension'
+            }),
 
-    hatGreaterThanPredicate (args, util) {
-        const option = Cast.toString(args.WHENGREATERTHANMENU).toLowerCase();
-        const value = Cast.toNumber(args.VALUE);
-        switch (option) {
-        case 'timer':
-            return util.ioQuery('clock', 'projectTimer') > value;
-        case 'loudness':
-            return this.runtime.audioEngine && this.runtime.audioEngine.getLoudness() > value;
-        }
-        return false;
-    }
+            // Optional: URI for a block icon, to display at the edge of each block for this
+            // extension. Data URI OK.
+            // TODO: what file types are OK? All web images? Just PNG?
+            blockIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
 
-    broadcast (args, util) {
-        const broadcastVar = util.runtime.getTargetForStage().lookupBroadcastMsg(
-            args.BROADCAST_OPTION.id, args.BROADCAST_OPTION.name);
-        if (broadcastVar) {
-            const broadcastOption = broadcastVar.name;
-            util.startHats('event_whenbroadcastreceived', {
-                BROADCAST_OPTION: broadcastOption
-            });
-        }
-    }
+            // Optional: URI for an icon to be displayed in the blocks category menu.
+            // If not present, the menu will display the block icon, if one is present.
+            // Otherwise, the category menu shows its default filled circle.
+            // Data URI OK.
+            // TODO: what file types are OK? All web images? Just PNG?
+            menuIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
 
-    broadcastAndWait (args, util) {
-        if (!util.stackFrame.broadcastVar) {
-            util.stackFrame.broadcastVar = util.runtime.getTargetForStage().lookupBroadcastMsg(
-                args.BROADCAST_OPTION.id, args.BROADCAST_OPTION.name);
-        }
-        if (util.stackFrame.broadcastVar) {
-            const broadcastOption = util.stackFrame.broadcastVar.name;
-            // Have we run before, starting threads?
-            if (!util.stackFrame.startedThreads) {
-                // No - start hats for this broadcast.
-                util.stackFrame.startedThreads = util.startHats(
-                    'event_whenbroadcastreceived', {
-                        BROADCAST_OPTION: broadcastOption
-                    }
-                );
-                if (util.stackFrame.startedThreads.length === 0) {
-                    // Nothing was started.
-                    return;
+            // Optional: Link to documentation content for this extension.
+            // If not present, offer no link.
+            docsURI: 'https://....',
+
+            // Required: the list of blocks implemented by this extension,
+            // in the order intended for display.
+            blocks: [{
+                    // Required: the machine-readable name of this operation.
+                    // This will appear in project JSON.
+                    opcode: 'myReporter', // becomes 'someBlocks.myReporter'
+
+                    // Required: the kind of block we're defining, from a predefined list.
+                    // Fully supported block types:
+                    //   BlockType.BOOLEAN - same as REPORTER but returns a Boolean value
+                    //   BlockType.COMMAND - a normal command block, like "move {} steps"
+                    //   BlockType.HAT - starts a stack if its value changes from falsy to truthy ("edge triggered")
+                    //   BlockType.REPORTER - returns a value, like "direction"
+                    // Block types in development or for internal use only:
+                    //   BlockType.BUTTON - place a button in the block palette
+                    //   BlockType.CONDITIONAL - control flow, like "if {}" or "if {} else {}"
+                    //     A CONDITIONAL block may return the one-based index of a branch to
+                    //     run, or it may return zero/falsy to run no branch.
+                    //   BlockType.EVENT - starts a stack in response to an event (full spec TBD)
+                    //   BlockType.LOOP - control flow, like "repeat {} {}" or "forever {}"
+                    //     A LOOP block is like a CONDITIONAL block with two differences:
+                    //     - the block is assumed to have exactly one child branch, and
+                    //     - each time a child branch finishes, the loop block is called again.
+                    blockType: BlockType.REPORTER,
+
+                    // Required for CONDITIONAL blocks, ignored for others: the number of
+                    // child branches this block controls. An "if" or "repeat" block would
+                    // specify a branch count of 1; an "if-else" block would specify a
+                    // branch count of 2.
+                    // TODO: should we support dynamic branch count for "switch"-likes?
+                    branchCount: 0,
+
+                    // Optional, default false: whether or not this block ends a stack.
+                    // The "forever" and "stop all" blocks would specify true here.
+                    terminal: true,
+
+                    // Optional, default false: whether or not to block all threads while
+                    // this block is busy. This is for things like the "touching color"
+                    // block in compatibility mode, and is only needed if the VM runs in a
+                    // worker. We might even consider omitting it from extension docs...
+                    blockAllThreads: false,
+
+                    // Required: the human-readable text on this block, including argument
+                    // placeholders. Argument placeholders should be in [MACRO_CASE] and
+                    // must be [ENCLOSED_WITHIN_SQUARE_BRACKETS].
+                    text: formatMessage({
+                        id: 'myReporter',
+                        defaultMessage: 'letter [LETTER_NUM] of [TEXT]',
+                        description: 'Label on the "myReporter" block'
+                    }),
+
+                    // Required: describe each argument.
+                    // Argument order may change during translation, so arguments are
+                    // identified by their placeholder name. In those situations where
+                    // arguments must be ordered or assigned an ordinal, such as interaction
+                    // with Scratch Blocks, arguments are ordered as they are in the default
+                    // translation (probably English).
+                    arguments: {
+                        // Required: the ID of the argument, which will be the name in the
+                        // args object passed to the implementation function.
+                        LETTER_NUM: {
+                            // Required: type of the argument / shape of the block input
+                            type: ArgumentType.NUMBER,
+
+                            // Optional: the default value of the argument
+                            default: 1
+                        },
+
+                        // Required: the ID of the argument, which will be the name in the
+                        // args object passed to the implementation function.
+                        TEXT: {
+                            // Required: type of the argument / shape of the block input
+                            type: ArgumentType.STRING,
+
+                            // Optional: the default value of the argument
+                            default: formatMessage({
+                                id: 'myReporter.TEXT_default',
+                                defaultMessage: 'text',
+                                description: 'Default for "TEXT" argument of "someBlocks.myReporter"'
+                            })
+                        }
+                    },
+
+                    // Optional: the function implementing this block.
+                    // If absent, assume `func` is the same as `opcode`.
+                    func: 'myReporter',
+
+                    // Optional: list of target types for which this block should appear.
+                    // If absent, assume it applies to all builtin targets -- that is:
+                    // [TargetType.SPRITE, TargetType.STAGE]
+                    filter: [TargetType.SPRITE]
+                },
+                {
+                    // Another block...
+                }
+            ],
+
+            // Optional: define extension-specific menus here.
+            menus: {
+                // Required: an identifier for this menu, unique within this extension.
+                menuA: [
+                    // Static menu: list items which should appear in the menu.
+                    {
+                        // Required: the value of the menu item when it is chosen.
+                        value: 'itemId1',
+
+                        // Optional: the human-readable label for this item.
+                        // Use `value` as the text if this is absent.
+                        text: formatMessage({
+                            id: 'menuA_item1',
+                            defaultMessage: 'Item One',
+                            description: 'Label for item 1 of menu A in "Some Blocks" extension'
+                        })
+                    },
+
+                    // The simplest form of a list item is a string which will be used as
+                    // both value and text.
+                    'itemId2'
+                ],
+
+                // Dynamic menu: returns an array as above.
+                // Called each time the menu is opened.
+                menuB: 'getItemsForMenuB',
+
+                // The examples above are shorthand for setting only the `items` property in this full form:
+                menuC: {
+                    // This flag makes a "droppable" menu: the menu will allow dropping a reporter in for the input.
+                    acceptReporters: true,
+
+                    // The `item` property may be an array or function name as in previous menu examples.
+                    items: [ /*...*/ ] || 'getItemsForMenuC'
+                }
+            },
+
+            // Optional: translations (UNSTABLE - NOT YET SUPPORTED)
+            translation_map: {
+                de: {
+                    'extensionName': 'Einige Blöcke',
+                    'myReporter': 'Buchstabe [LETTER_NUM] von [TEXT]',
+                    'myReporter.TEXT_default': 'Text',
+                    'menuA_item1': 'Artikel eins',
+
+                    // Dynamic menus can be translated too
+                    'menuB_example': 'Beispiel',
+
+                    // This message contains ICU placeholders (see `myReporter()` below)
+                    'myReporter.result': 'Buchstabe {LETTER_NUM} von {TEXT} ist {LETTER}.'
+                },
+                it: {
+                    // ...
                 }
             }
-            // We've run before; check if the wait is still going on.
-            const instance = this;
-            // Scratch 2 considers threads to be waiting if they are still in
-            // runtime.threads. Threads that have run all their blocks, or are
-            // marked done but still in runtime.threads are still considered to
-            // be waiting.
-            const waiting = util.stackFrame.startedThreads
-                .some(thread => instance.runtime.threads.indexOf(thread) !== -1);
-            if (waiting) {
-                // If all threads are waiting for the next tick or later yield
-                // for a tick as well. Otherwise yield until the next loop of
-                // the threads.
-                if (
-                    util.stackFrame.startedThreads
-                        .every(thread => instance.runtime.isWaitingThread(thread))
-                ) {
-                    util.yieldTick();
-                } else {
-                    util.yield();
-                }
-            }
-        }
-    }
+        };
+    };
+
+    /**
+     * Implement myReporter.
+     * @param {object} args - the block's arguments.
+     * @property {string} MY_ARG - the string value of the argument.
+     * @returns {string} a string which includes the block argument value.
+     */
+    myReporter(args) {
+        // This message contains ICU placeholders, not Scratch placeholders
+        const message = formatMessage({
+            id: 'myReporter.result',
+            defaultMessage: 'Letter {LETTER_NUM} of {TEXT} is {LETTER}.',
+            description: 'The text template for the "myReporter" block result'
+        });
+
+        // Note: this implementation is not Unicode-clean; it's just here as an example.
+        const result = args.TEXT.charAt(args.LETTER_NUM);
+
+        return message.format({
+            LETTER_NUM: args.LETTER_NUM,
+            TEXT: args.TEXT,
+            LETTER: result
+        });
+    };
 }
-
-module.exports = Scratch3EventBlocks;
