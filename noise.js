37be491535f058843d965d05949cffd6ebf3673c
Astro_815 <103153597+Astro815@users.noreply.github.com>
2023-02-11 20:37:26 -0300

    Update noise.js

--- noise.js
+++ noise.js
@@ -1,121 +1,439 @@
-class PerlinNoise {
+// Some parts of this scripts are based on or designed to be compatible-ish with:
+// https://arpruss.github.io/gamepad.js (MIT Licensed)
 
-    /// perlin noise (raw)
-    ///
-    /// - Parameters:
-    ///     - x: The *x* component of the vector.
-    ///     - y: The *y* component of the vector.
-    ///     - z: The *z* component of the vector.
-    /// - Returns: noise value (-1.0 ≦ noise ≦ +1.0)
-    noise(x, y, z) {
-        const xi = Math.floor(x) & 255
-        const yi = Math.floor(y) & 255
-        const zi = Math.floor(z) & 255
-        const xf = x - Math.floor(x)
-        const yf = y - Math.floor(y)
-        const zf = z - Math.floor(z)
-        const u = this.fade(xf)
-        const v = this.fade(yf)
-        const w = this.fade(zf)
-        const aaa = this.p[this.p[this.p[xi    ] + yi    ] + zi    ]
-        const aba = this.p[this.p[this.p[xi    ] + yi + 1] + zi    ]
-        const aab = this.p[this.p[this.p[xi    ] + yi    ] + zi + 1]
-        const abb = this.p[this.p[this.p[xi    ] + yi + 1] + zi + 1]
-        const baa = this.p[this.p[this.p[xi + 1] + yi    ] + zi    ]
-        const bba = this.p[this.p[this.p[xi + 1] + yi + 1] + zi    ]
-        const bab = this.p[this.p[this.p[xi + 1] + yi    ] + zi + 1]
-        const bbb = this.p[this.p[this.p[xi + 1] + yi + 1] + zi + 1]
-        return this.lerp(w,
-                    this.lerp(v,
-                         this.lerp(u,
-                              this.grad(aaa, xf,     yf,     zf),
-                              this.grad(baa, xf - 1, yf,     zf)),
-                         this.lerp(u,
-                              this.grad(aba, xf,     yf - 1, zf),
-                              this.grad(bba, xf - 1, yf - 1, zf))),
-                    this.lerp(v,
-                         this.lerp(u,
-                              this.grad(aab, xf,     yf,     zf - 1),
-                              this.grad(bab, xf - 1, yf,     zf - 1)),
-                         this.lerp(u,
-                              this.grad(abb, xf,     yf - 1, zf - 1),
-                              this.grad(bbb, xf - 1, yf - 1, zf - 1))))
-    }
-    fade(t) { return t * t * t * (t * (t * 6 - 15) + 10); }
-    lerp(t, a, b) { return a + t * (b - a); }
-    grad(hash, x, y, z) {
-        switch (hash & 15) {
-        case 0: return  x + y;
-        case 1: return -x + y;
-        case 2: return  x - y;
-        case 3: return -x - y;
-        case 4: return  x + z;
-        case 5: return -x + z;
-        case 6: return  x - z;
-        case 7: return -x - z;
-        case 8: return  y + z;
-        case 9: return -y + z;
-        case 10: return  y - z;
-        case 11: return -y - z;
-        case 12: return  y + x;
-        case 13: return -y + z;
-        case 14: return  y - x;
-        case 15: return -y - z;
-        default: return 0; // never happens
-        }
-    }
+(function(Scratch) {
+  'use strict';
 
-    constructor() {
-        const permutation = [
-            151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7, 225,
-            140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23, 190, 6, 148,
-            247, 120, 234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32,
-            57, 177, 33, 88, 237, 149, 56, 87, 174, 20, 125, 136, 171, 168, 68,
-            175, 74, 165, 71, 134, 139, 48, 27, 166, 77, 146, 158, 231, 83, 111,
-            229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40, 244,
-            102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208,
-            89, 18, 169, 200, 196, 135, 130, 116, 188, 159, 86, 164, 100, 109,
-            198, 173, 186, 3, 64, 52, 217, 226, 250, 124, 123, 5, 202, 38, 147,
-            118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17, 182,
-            189, 28, 42, 223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70,
-            221, 153, 101, 155, 167, 43, 172, 9, 129, 22, 39, 253, 19, 98, 108,
-            110, 79, 113, 224, 232, 178, 185, 112, 104, 218, 246, 97, 228, 251,
-            34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235,
-            249, 14, 239, 107, 49, 192, 214, 31, 181, 199, 106, 157, 184, 84, 204,
-            176, 115, 121, 50, 45, 127, 4, 150, 254, 138, 236, 205, 93, 222, 114,
-            67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180
-        ];
-        this.p = permutation.concat(permutation);
-    }
-}
+  const DEADZONE = 0.1;
 
-class noise {
+  /**
+   * @param {number|'any'} index 1-indexed index
+   * @returns {Gamepad[]}
+   */
+  const getGamepads = (index) => {
+    if (index === 'any') {
+      return navigator.getGamepads().filter(i => i);
+    }
+    const gamepad = navigator.getGamepads()[index - 1];
+    if (gamepad) {
+      return [gamepad];
+    }
+    return [];
+  };
+
+  /**
+   * @param {Gamepad} gamepad
+   * @param {number|'any'} buttonIndex 1-indexed index
+   * @returns {boolean} false if button does not exist
+   */
+  const isButtonPressed = (gamepad, buttonIndex) => {
+    if (buttonIndex === 'any') {
+      return gamepad.buttons.some(i => i.pressed);
+    }
+    const button = gamepad.buttons[buttonIndex - 1];
+    if (!button) {
+      return false;
+    }
+    return button.pressed;
+  };
+
+  /**
+   * @param {Gamepad} gamepad
+   * @param {number} axisIndex 1-indexed index
+   * @returns {number} 0 if axis does not exist
+   */
+  const getAxisValue = (gamepad, axisIndex) => {
+    const axisValue = gamepad.axes[axisIndex - 1];
+    if (typeof axisValue !== 'number') {
+      return 0;
+    }
+    if (Math.abs(axisValue) < DEADZONE) {
+      return 0;
+    }
+    return axisValue;
+  };
+
+  class GamepadExtension {
     getInfo() {
-        return {
-            id: 'NoiseJsScratch',
-            name: 'Noise',
-            blocks: [{
-                    opcode: 'noise',
-                    blockType: Scratch.BlockType.REPORTER,
-                    text: 'Noise x: [x] y: [y]',
-                    arguments: {
-                        x: {
-                            type: Scratch.ArgumentType.NUMBER,
-                            defaultValue: '0'
-                        },y: {
-                            type: Scratch.ArgumentType.NUMBER,
-                            defaultValue: '0'
-                        }
-                    }
-                }]
-        };
+      return {
+        id: 'Gamepad',
+        name: 'Gamepad',
+        blocks: [
+          {
+            opcode: 'gamepadConnected',
+            blockType: Scratch.BlockType.BOOLEAN,
+            text: 'is gamepad [pad] connected?',
+            arguments: {
+              pad: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: '1',
+                menu: 'padMenu'
+              }
+            }
+          },
+          {
+            opcode: 'buttonDown',
+            blockType: Scratch.BlockType.BOOLEAN,
+            text: 'button [b] of pad [i] pressed?',
+            arguments: {
+              b: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: '1',
+                menu: 'buttonMenu'
+              },
+              i: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: '1',
+                menu: 'padMenu'
+              }
+            }
+          },
+          {
+            opcode: 'axisValue',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'value of axis [b] on pad [i]',
+            arguments: {
+              b: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: '1',
+                menu: 'axisMenu'
+              },
+              i: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: '1',
+                menu: 'padMenu'
+              },
+            },
+          },
 
+          '---',
+
+          {
+            opcode: 'axisDirection',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'direction of axes [axis] on pad [pad]',
+            arguments: {
+              axis: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: '1',
+                menu: 'axesGroupMenu'
+              },
+              pad: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: '1',
+                menu: 'padMenu'
+              }
+            }
+          },
+          {
+            opcode: 'axisMagnitude',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'magnitude of axes [axis] on pad [pad]',
+            arguments: {
+              axis: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: '1',
+                menu: 'axesGroupMenu'
+              },
+              pad: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: '1',
+                menu: 'padMenu'
+              }
+            }
+          },
+
+          /*
+          {
+            opcode: 'buttonPressedReleased',
+            blockType: Scratch.BlockType.HAT,
+            text: 'button [b] [pr] of pad [i]',
+            arguments: {
+              b: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: '1'
+              },
+              pr: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: '1',
+                menu: 'pressReleaseMenu'
+              },
+              i: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: '1',
+                menu: 'padMenu'
+              },
+            },
+          },
+
+          {
+            opcode: 'axisMoved',
+            blockType: Scratch.BlockType.HAT,
+            text: 'axis [b] of pad [i] moved',
+            arguments: {
+              b: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: '1'
+              },
+              i: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: '1',
+                menu: 'padMenu'
+              },
+            },
+          },
+          */
+
+          '---',
+
+          {
+            opcode: 'rumble',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'rumble strong [s] and weak [w] for [t] sec. on pad [i]',
+            arguments: {
+              s: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: '0.25'
+              },
+              w: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: '0.5'
+              },
+              t: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: '0.25'
+              },
+              i: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: '1',
+                menu: 'padMenu'
+              },
+            },
+          },
+        ],
+        menus: {
+          padMenu: {
+            acceptReporters: true,
+            items: [
+              {
+                text: 'any',
+                value: 'any'
+              },
+              {
+                text: '1',
+                value: '1'
+              },
+              {
+                text: '2',
+                value: '2'
+              },
+              {
+                text: '3',
+                value: '3'
+              },
+              {
+                text: '4',
+                value: '4'
+              }
+            ],
+          },
+          buttonMenu: {
+            acceptReporters: true,
+            items: [
+              // Based on an Xbox controller
+              {
+                text: 'any',
+                value: 'any'
+              },
+              {
+                text: 'A (1)',
+                value: '1'
+              },
+              {
+                text: 'B (2)',
+                value: '2'
+              },
+              {
+                text: 'X (3)',
+                value: '3'
+              },
+              {
+                text: 'Y (4)',
+                value: '4'
+              },
+              {
+                text: 'Left bumper (5)',
+                value: '5'
+              },
+              {
+                text: 'Right bumper (6)',
+                value: '6'
+              },
+              {
+                text: 'Left trigger (7)',
+                value: '7'
+              },
+              {
+                text: 'Right trigger (8)',
+                value: '8'
+              },
+              {
+                text: 'Select/View (9)',
+                value: '9'
+              },
+              {
+                text: 'Start/Menu (10)',
+                value: '10'
+              },
+              {
+                text: 'Left stick (11)',
+                value: '11'
+              },
+              {
+                text: 'Right stick (12)',
+                value: '12'
+              },
+              {
+                text: 'D-pad up (13)',
+                value: '13'
+              },
+              {
+                text: 'D-pad down (14)',
+                value: '14'
+              },
+              {
+                text: 'D-pad left (15)',
+                value: '15'
+              },
+              {
+                text: 'D-pad right (16)',
+                value: '16'
+              },
+            ]
+          },
+          axisMenu: {
+            acceptReporters: true,
+            items: [
+              // Based on an Xbox controller
+              {
+                text: 'Left stick horizontal (1)',
+                value: '1'
+              },
+              {
+                text: 'Left stick vertical (2)',
+                value: '2'
+              },
+              {
+                text: 'Right stick horizontal (3)',
+                value: '3'
+              },
+              {
+                text: 'Right stick vertical (4)',
+                value: '4'
+              }
+            ]
+          },
+          axesGroupMenu: {
+            acceptReporters: true,
+            items: [
+              // Based on an Xbox controller
+              {
+                text: 'Left stick (1 & 2)',
+                value: '1'
+              },
+              {
+                text: 'Right stick (3 & 4)',
+                value: '3'
+              }
+            ]
+          },
+          /*
+          pressReleaseMenu: [
+            {
+              text: 'press',
+              value: 1
+            },
+            {
+              text: 'release',
+              value: 0
+            }
+          ],
+          */
+        }
+      };
     }
 
-    noise({x, y}) {
-        return PerlinNoise.noise(x,y,0);
-    };
+    gamepadConnected ({pad}) {
+      return getGamepads(pad).length > 0;
+    }
 
-}
+    buttonDown ({b, i}) {
+      for (const gamepad of getGamepads(i)) {
+        if (isButtonPressed(gamepad, b)) {
+          return true;
+        }
+      }
+      return false;
+    }
 
-Scratch.extensions.register(new noise());
+    axisValue ({b, i}) {
+      let greatestAxis = 0;
+      for (const gamepad of getGamepads(i)) {
+        const axis = getAxisValue(gamepad, b);
+        if (Math.abs(axis) > Math.abs(greatestAxis)) {
+          greatestAxis = axis;
+        }
+      }
+      return greatestAxis;
+    }
+
+    axisDirection ({axis, pad}) {
+      let greatestMagnitude = 0;
+      let direction = 90;
+      for (const gamepad of getGamepads(pad)) {
+        const horizontalAxis = getAxisValue(gamepad, axis);
+        const verticalAxis = getAxisValue(gamepad, +axis + 1);
+        const magnitude = Math.sqrt(horizontalAxis ** 2 + verticalAxis ** 2);
+        if (magnitude > greatestMagnitude) {
+          greatestMagnitude = magnitude;
+          direction = Math.atan2(verticalAxis, horizontalAxis) * 180 / Math.PI + 90;
+          if (direction < 0) {
+            direction += 360;
+          }
+        }
+      }
+      return direction;
+    }
+
+    axisMagnitude ({axis, pad}) {
+      let greatestMagnitude = 0;
+      for (const gamepad of getGamepads(pad)) {
+        const horizontalAxis = getAxisValue(gamepad, axis);
+        const verticalAxis = getAxisValue(gamepad, +axis + 1);
+        const magnitude = Math.sqrt(horizontalAxis ** 2 + verticalAxis ** 2);
+        if (magnitude > greatestMagnitude) {
+          greatestMagnitude = magnitude;
+        }
+      }
+      return greatestMagnitude;
+    }
+
+    rumble ({s, w, t, i}) {
+      const gamepads = getGamepads(i);
+      for (const gamepad of gamepads) {
+        // @ts-ignore
+        if (gamepad.vibrationActuator) {
+          // @ts-ignore
+          gamepad.vibrationActuator.playEffect('dual-rumble', {
+            startDelay: 0,
+            duration: t * 1000,
+            weakMagnitude: w,
+            strongMagnitude: s
+          });
+        }
+      }
+    }
+  }
+
+  Scratch.extensions.register(new GamepadExtension());
+})(Scratch);
